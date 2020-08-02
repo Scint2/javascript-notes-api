8802c7bfcd04cb20eb1ce9574eea54cd608ce3b9
Scint2 <69097013+Scint2@users.noreply.github.com>
2020-08-02 00:37:32 -0300

    Update users.js

--- app/routes/users.js
+++ app/routes/users.js
@@ -17,7 +17,7 @@ router.post('/register', async (req, res) => {
     await user.save()
     res.status(200).json(user)
   } catch (error) {
-    res.status(500).json({ error: 'Error registering new user' })
+    res.status(500).json({ error: error })
   }
 
 })
